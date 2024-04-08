e6112116a11a64032f6ad7a5b77906007fa921a6
Ulvi Azizli <ulviazizli96@gmail.com>
2024-04-08 17:49:24 +0200

    done 1,2

--- src/index.js
+++ src/index.js
@@ -7,13 +7,35 @@ let timer = null; // Variable to store the interval
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
-
+const startBtn = document.getElementById("start-btn");
+startBtn.addEventListener("click", () => {
+  startCountdown();
+});
 
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
+
+  document.getElementById("start-btn").disabled = true;
+
+  remainingTime = 10;
+
+  const intervalId = setInterval(() => {
+    remainingTime--;
+
+    const remainTime = document.getElementById("time");
+    remainTime.textContent = remainingTime;
+
+    if(remainingTime === 0) {
+      clearInterval(intervalId);
+
+      showToast();
+      document.getElementById("start-btn").disabled = false;
+
+    }
+  }, 1000);
 
 
   // Your code goes here ...
